fb05a5c5c79404fcdf0b7bf1e79556345f8e0753
asiersanzmedina-lab <asiersanzmedina@gmail.com>
2026-09-11 11:18:02 +0200

    first commit

---
+++ src/javafundamentals1/Javafundamentals1.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class VariablesTest {
+	
+	String name = "Nestor";
+	public static void main(String[] args) {
+		System.out.println(1223123);
+		System.out.println("Hello Java");
+		name = "Asier";
+		System.out.println(name);
+	{
+}
