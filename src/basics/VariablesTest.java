0b0cb4c2bb4053c209d06767482d7e36d627cec6
asiersanzmedina-lab <asiersanzmedina@gmail.com>
2026-09-15 09:56:38 +0200

    first commit

---
+++ src/basics/VariablesTest.java
@@ -0,0 +1,24 @@
+package basics;
+
+public class VariablesTest {
+	
+	String name = "Nestor";
+	public static void main(String[] args) {
+		System.out.println(1223123);
+		System.out.println("Hello Java");
+		name = "Asier";
+		System.out.println(name);
+		int age = 23432432;
+		float height = 23.3f;
+		byte a = -127;
+	
+		age = age + 10;
+		System.out.println(age);
+		age = age * 10;
+		System.out.println(age);
+		weight = weight - 100000;
+		System.out.println(age);
+		heihgt = height / 100;
+		System.out.println(height);
+		
+}
