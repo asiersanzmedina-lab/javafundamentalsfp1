d84d56c3b1e5b52efe0568453e5376fa3897f0bb
asiersanzmedina-lab <asiersanzmedina@gmail.com>
2026-09-17 10:54:47 +0200

    Actualiza clases todo correcto

--- src/basics/ConditionalTest2.java
+++ src/basics/ConditionalTest2.java
@@ -2,4 +2,34 @@ package basics;
 
 public class ConditionalTest2 {
 
+	public static void main(String[] args) {
+
+		// step 1: crear variables para almacenar información
+		float score = 150;
+		// step 2: modelar el escenario de diferentes casos using condicionales
+		if (score >= 100) {
+			System.out.println("No, that score is not possible!!!!");
+		} else {
+			System.out.println("I am going to have a nice afternoon!!!");
+		}
+
+		// usar if, else if, else para modelar a scenario in wich
+		// the cases are exclusive among them
+		score = 70;
+		if (score >= 100) {
+			System.out.println("No, that score is not possible!!!!");
+		} else if (score < 100 && score > 60) {
+			System.out.println("The score is high, I am going to pass the exam");
+		} else {
+			System.out.println("I am going to have a nice afternoon!!!");
+		}
+
+		// robotaxi
+		boolean isAdvancing = true;
+
+		if (isAdvancing) {
+			System.out.println("El robotaxi está avanzando...");
+		}
+	}
+
 }
--- src/basics/VariablesTest.java
+++ src/basics/VariablesTest.java
@@ -10,7 +10,8 @@ public class VariablesTest {
 		System.out.println(name);
 		int age = 23432432;
 		float height = 23.3f;
-		double b
+		double b;
+		float weight = 34f;
 		byte a = -127;
 	
 		age = age + 10;
@@ -19,18 +20,17 @@ public class VariablesTest {
 		System.out.println(age);
 		weight = weight - 100000;
 		System.out.println(age);
-		heihgt = height / 100;
+		height = height / 100;
 		System.out.println(height);
 		
 		a = 3;
 		b = 4;
 		System.out.println("the square of a = " + a * a);
 		System.out.println("the square of a = " + b * b);
-		System.out.println("the square of c = " a * a + b * b);
+		System.out.println("the square of c = " + a * a + b * b);
 		
 		final int SPEED_OF_LIGHT = 299000000;
 		float mass = 2.5f;
 		//SPEED_OF_LIGHT = 234;
-		
-		
+	}			
 }
