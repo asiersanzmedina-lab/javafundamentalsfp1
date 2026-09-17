eee8d31dadeae303fe3ec2ead7a72ee10d23e0ef
asiersanzmedina-lab <asiersanzmedina@gmail.com>
2026-09-17 10:34:21 +0200

    Agrega ConditionalTest, Conditionaltest2 y VariablesTest

---
+++ src/basics/VariablesTest.java
@@ -0,0 +1,36 @@
+package basics;
+
+public class VariablesTest {
+	
+	String name = "Nestor";
+	public static void main(String[] args) {
+		System.out.println(1223123);
+		System.out.println("Hello Java");
+		String name = "Asier";
+		System.out.println(name);
+		int age = 23432432;
+		float height = 23.3f;
+		double b
+		byte a = -127;
+	
+		age = age + 10;
+		System.out.println(age);
+		age = age * 10;
+		System.out.println(age);
+		weight = weight - 100000;
+		System.out.println(age);
+		heihgt = height / 100;
+		System.out.println(height);
+		
+		a = 3;
+		b = 4;
+		System.out.println("the square of a = " + a * a);
+		System.out.println("the square of a = " + b * b);
+		System.out.println("the square of c = " a * a + b * b);
+		
+		final int SPEED_OF_LIGHT = 299000000;
+		float mass = 2.5f;
+		//SPEED_OF_LIGHT = 234;
+		
+		
+}
