04c692c7fdeebe440119a8000ec62cd9c8559668
asiersanzmedina-lab <asiersanzmedina@gmail.com>
2026-09-22 11:43:46 +0200

    Loop 4

--- src/basics/ForLoop3.java
+++ src/basics/ForLoop3.java
@@ -1,18 +1,36 @@
 package basics;
-
+/**
+ * Sep 22/09
+ * @author asier
+ *
+ */
 public class ForLoop3 {
 	
 	public static void main(String[] args) {
 //		Print all evem numbers between 5 and 102
-		for (int i = 4; i <= 102; i = i + 2) {
-		System.out.println(i);
+//		for (int i = 4; i <= 102; i = i + 1) {
+//		System.out.println(i);
+//		}
+
+//		Print all evem numbers between 5 and 102
+		for (int i = 5; i <= 102; i = i + 1) {
+//			if the value of i is even, we print it
+			if(i % 2 == 0) {
+			System.out.println(i);
+			}
 		}
 		
 //		Print all evem numbers between 5 and 102
 		for (int i = 5; i <= 102; i = i + 1) {
 //			if the value of i is even, we print it
-			if(102 % 2 == 0) {
-			System.out.println(i);
+			/**	"i % 2 =0 1"
+			 * is equivalent to
+			 * "i % 2 != 0"
+			 * "!=" means not equal
+			 * "==" means equal
+			 */
+			if(i % 2 != 0) {
+				System.out.println(i);
 			}
 		}
 	}
